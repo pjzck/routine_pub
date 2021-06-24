2c84ddece06572b66fe321d765a6f46fe9de8db1
pjzck.heimao <pjzck@zju.edu.cn>
2021-06-25 01:44:06 +0800

    update

--- api/hello_world.py
+++ api/hello_world.py
@@ -3,5 +3,5 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(("AutoBuild : Hello " + self.headers.get('x-forwarded-for')).encode())
+        # self.wfile.write(("AutoBuild : Hello " + self.headers.get('x-forwarded-for')).encode())
         return
